7fb793b735197f10fd5ea557a539735b8d98b8a9
Unknown <kurobyte@gmail.com>
2018-04-22 01:07:28 +0200

    Canvi de color

--- Assets/scripts/InitScene.cs
+++ Assets/scripts/InitScene.cs
@@ -20,15 +20,17 @@ public class InitScene : MonoBehaviour {
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
                 cubes[i][j] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cubes[i][j].transform.localScale = new Vector3(fieldSize, fieldSize);
+                //cubes[i][j].transform.localScale = new Vector3(fieldSize, fieldSize);
                 cubes[i][j].transform.position = new Vector3(-8.39F + i * fieldSize, 4.5F - j * fieldSize);
             }
         }
 
+        cubes[0][0].GetComponent<Renderer>().material.color = Color.red;
+
         //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         //cube.transform.position = new Vector3(0, 0, 0);
 
-        
+
     }
 	
 	// Update is called once per frame
