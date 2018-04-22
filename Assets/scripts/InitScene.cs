5f774ce50ec4640ed66185d9cea5e2010b4eda9e
Unknown <kurobyte@gmail.com>
2018-04-22 02:06:14 +0200

    Generacio de posicions aleatories

--- Assets/scripts/InitScene.cs
+++ Assets/scripts/InitScene.cs
@@ -9,11 +9,12 @@ public class InitScene : MonoBehaviour {
     const float fieldSize = 1F;
 
     GameObject sceneCamObj;
+    GameObject[][] cubes;
 
     // Use this for initialization
     void Start () {
         Screen.SetResolution(1280, 720, true);
-        GameObject[][] cubes = new GameObject[ROWS][];
+        cubes = new GameObject[ROWS][];
 
         for (int i = 0; i < ROWS; i++) { cubes[i] = new GameObject[COLS]; }
 
@@ -25,16 +26,84 @@ public class InitScene : MonoBehaviour {
             }
         }
 
-        cubes[0][0].GetComponent<Renderer>().material.color = Color.red;
-
-        //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        //cube.transform.position = new Vector3(0, 0, 0);
-
-
+        //cubes[0][0].GetComponent<Renderer>().material.color = Color.red;
+        genPath();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    void genPath() {
+        //Set Start
+        Vector3 sPos = setStartPoint();
+        cubes[(int)sPos.x][(int)sPos.y].GetComponent<Renderer>().material.color = Color.red;
+        //Set Castle
+        Vector2 fPos = setEndPoint((int)sPos.z);
+        cubes[(int)fPos.x][(int)fPos.y].GetComponent<Renderer>().material.color = Color.green;
+        //GEN PATH
+    }
+
+    Vector3 setStartPoint() {
+        Vector3 v = new Vector3();
+        Random r = new Random();
+        int zone = Random.Range(0, 3);
+        int pos;
+
+        switch (zone) {
+            case 0: //top
+                pos = Random.Range(0, ROWS - 1);
+                v.Set(pos, 0, zone);
+                break;
+            case 1: //Right
+                pos = Random.Range(0, COLS - 1);
+                v.Set(COLS - 1, pos, zone);
+                break;
+            case 2: //Down
+                pos = Random.Range(0, ROWS - 1);
+                v.Set(pos, ROWS - 1, zone);
+                break;
+            case 3: //top
+                pos = Random.Range(0, COLS - 1);
+                v.Set(0, pos, zone);
+                break;
+        }
+
+        return v;
+    }
+
+    Vector2 setEndPoint(int startZ)
+    {
+        Vector2 v = new Vector2();
+        int zone;
+
+        do {
+            zone = Random.Range(0, 3);
+        } while (zone == startZ);
+        
+        int pos;
+
+        switch (zone)
+        {
+            case 0: //top
+                pos = Random.Range(0, ROWS - 1);
+                v.Set(pos, 0);
+                break;
+            case 1: //Right
+                pos = Random.Range(0, COLS - 1);
+                v.Set(COLS - 1, pos);
+                break;
+            case 2: //Down
+                pos = Random.Range(0, ROWS - 1);
+                v.Set(pos, ROWS - 1);
+                break;
+            case 3: //top
+                pos = Random.Range(0, COLS - 1);
+                v.Set(0, pos);
+                break;
+        }
+
+        return v;
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
